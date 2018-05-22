d087723702e70efd98fd83470d20bb7ec0085794
phiquang <phiquang94@gmail.com>
2018-05-22 23:09:32 +0700

    ver-1-1

--- js/function.js
+++ js/function.js
@@ -6,3 +6,28 @@ function openNav() {
 function closeNav() {
    	document.getElementById("mySidenav").style.width = "0";
 }
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo-min");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+         $(function() {
+            $( "#slider-3" ).slider({
+               range:true,
+               min: 0,
+               max: 500,
+               values: [ 35, 200 ],
+               slide: function( event, ui ) {
+                  $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+               }
+            });
+            $( "#price" ).val( "$" + $( "#slider-3" ).slider( "values", 0 ) +
+               " - $" + $( "#slider-3" ).slider( "values", 1 ) );
+         });
+      
